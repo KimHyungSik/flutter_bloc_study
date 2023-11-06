f9b96a39e78e9699c02c7e4021247273c1f51e42
KimHyungSik <colrog968@gmail.com>
2023-11-06 23:24:47 +0900

    timer bloc 정의

--- lib/timer/bloc/timer_bloc_bloc.dart
+++ lib/timer/bloc/timer_bloc_bloc.dart
@@ -1,13 +1,64 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_study/timer/bloc/timer_state.dart';
+import 'package:bloc_study/timer/ticker.dart';
 import 'package:meta/meta.dart';
 
 part 'timer_bloc_event.dart';
-part 'timer_bloc_state.dart';
 
-class TimerBlocBloc extends Bloc<TimerBlocEvent, TimerBlocState> {
-  TimerBlocBloc() : super(TimerBlocInitial()) {
-    on<TimerBlocEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  final Ticker _ticker;
+  static const int _duration = 60;
+
+  StreamSubscription<int>? _tickerSubscription;
+
+  TimerBloc({required ticker})
+      : _ticker = ticker,
+        super(TimerInitial(_duration)) {
+    on<TimerStarted>(_onStarted);
+    on<TimerPaused>(_onPaused);
+    on<TimerResumed>(_onResumed);
+    on<TimerReset>(_onReset);
+    on<_TimerTicked>(_onTicked);
+  }
+
+  @override
+  Future<void> close() {
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  void _onStarted(TimerStarted event, Emitter<TimerState> emit) {
+    emit(TimerRunInProgress(event.duration));
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker
+        .tick(ticks: event.duration)
+        .listen((duration) => add(_TimerTicked(duration: duration)));
+  }
+
+  void _onTicked(_TimerTicked event, Emitter<TimerState> emit) {
+    emit(event.duration > 0
+        ? TimerRunInProgress(event.duration)
+        : TimerRunComplete());
+  }
+
+  void _onPaused(TimerPaused event, Emitter<TimerState> emit) {
+    if (state is TimerRunInProgress) {
+      _tickerSubscription?.pause();
+      emit(TimerRunPause(state.duration));
+    }
+  }
+
+  void _onReset(TimerReset event, Emitter<TimerState> emit) {
+    _tickerSubscription?.cancel();
+    emit(const TimerInitial(_duration));
+  }
+
+  void _onResumed(TimerResumed resume, Emitter<TimerState> emit) {
+    if (state is TimerRunPause) {
+      _tickerSubscription?.resume();
+      emit(TimerRunInProgress(state.duration));
+    }
   }
 }
