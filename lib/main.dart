c9384690c626ee737903b2590b40106c8aed7249
KimHyungSik <colrog968@gmail.com>
2023-11-06 23:36:20 +0900

    timer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_study/timer/timer_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,5 +7,5 @@ import ' counter/counter_app.dart';
 
 void main() {
   Bloc.observer = const CounterObserver();
-  runApp(const CounterApp());
+  runApp(const TimerApp());
 }
---
+++ lib/timer/timer_page.dart
@@ -0,0 +1,80 @@
+import 'package:bloc_study/timer/ticker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'actions.dart';
+import 'background.dart';
+import 'bloc/timer_bloc_bloc.dart';
+
+class TimerApp extends StatelessWidget {
+  const TimerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Timer',
+      theme: ThemeData(
+        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+        colorScheme: const ColorScheme.light(
+          secondary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+      home: const TimerPage(),
+    );
+  }
+}
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => TimerBloc(ticker: Ticker()), child: const TimerView());
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer')),
+      body: Stack(
+        children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(
+                  child: TimerText(),
+                ),
+              ),
+              TimerAction(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr =
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minutesStr:$secondsStr',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
