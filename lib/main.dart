d48694ef9fc75dc08f8399291d4acf4d51dab63c
KimHyungSik <colrog968@gmail.com>
2023-11-07 23:34:40 +0900

    post page 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:bloc_study/intinite_list/post_page.dart';
 import 'package:bloc_study/timer/timer_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import ' counter/bloc/counter_observer.dart';
 import ' counter/counter_app.dart';
+import 'intinite_list/bloc/simple_bloc_observer.dart';
 
 void main() {
   Bloc.observer = const CounterObserver();
-  runApp(const TimerApp());
+  Bloc.observer = const SimpleBlocObserver();
+  runApp(const PostApp());
 }
