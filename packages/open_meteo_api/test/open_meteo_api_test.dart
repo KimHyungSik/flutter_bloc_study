b1064936c5b5767bc8c5955664a1c605aaac776f
KimHyungSik <colrog968@gmail.com>
2023-11-14 22:58:30 +0900

    open_meteo_api package

---
+++ packages/open_meteo_api/test/open_meteo_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:open_meteo_api/open_meteo_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
